11cad595debe9bbdbd374b50aa3c34b9026c81b5
gyri11212-art <gyri11212@gmail.com>
2026-08-09 17:45:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def generate_analytical_digest(raw_data):
     """
     
     response = client.models.generate_content(
-        model='gemini-1.5-flash',
+        model='gemini-2.0-flash',
         contents=prompt
     )
     return response.text
